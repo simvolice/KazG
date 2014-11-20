95d2ff7698f3b1defa426aea5c92587318b367af
simvolice <simvolice@gmail.com>
2014-11-20 18:28:31 +0600

    Добавлены вьюхи

--- src/main/java/org/springframework/controller/MainController.java
+++ src/main/java/org/springframework/controller/MainController.java
@@ -1,9 +1,19 @@
 package org.springframework.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.service.MainService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Locale;
 
 /**
@@ -13,6 +23,16 @@ import java.util.Locale;
 public class MainController {
 
 
+    private final MainService mainService;
+
+   @Autowired
+   public MainController(MainService mainService) {
+
+        this.mainService = mainService;
+
+    }
+
+
     @RequestMapping("/")
     public String root(Locale locale) {
         return "redirect:/login";
@@ -20,28 +40,54 @@ public class MainController {
 
 
 
-    /** Home page. */
-    @RequestMapping("/index")
-    public String index() {
-        return "index";
+
+
+
+
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+                              @RequestParam(value = "logout", required = false) String logout,
+                              HttpServletRequest request) {
+
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username and password!");
+
+            //login form for update page
+            //if login error, get the targetUrl from session again.
+            String targetUrl = getRememberMeTargetUrlFromSession(request);
+            System.out.println(targetUrl);
+            if(StringUtils.hasText(targetUrl)){
+                model.addObject("targetUrl", targetUrl);
+                model.addObject("loginUpdate", true);
+            }
+
+        }
+
+        if (logout != null) {
+            model.addObject("msg", "You've been logged out successfully.");
+        }
+        model.setViewName("login");
+
+
+
+
+
+        return model;
+
     }
 
 
-    // Login form
-    @RequestMapping("/login")
-    public String login() {
-        return "login";
+    private String getRememberMeTargetUrlFromSession(HttpServletRequest request){
+        String targetUrl = "";
+        HttpSession session = request.getSession(false);
+        if(session!=null){
+            targetUrl = session.getAttribute("targetUrl")==null?""
+                    :session.getAttribute("targetUrl").toString();
+        }
+        return targetUrl;
     }
-
-    @RequestMapping("/login-error")
-    public String loginError(Model model) {
-        model.addAttribute("loginError", true);
-        return "login";
-    }
-
-
-
-
 
 
 
--- src/main/java/org/springframework/service/MainServiceImpl.java
+++ src/main/java/org/springframework/service/MainServiceImpl.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DocCreateUslDAO;
 import org.springframework.dao.DogovoraDAO;
 import org.springframework.model.DocCreateUslEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by user on 19.11.2014.
  */
+@Service
 public class MainServiceImpl implements MainService {
 
 
